0d87d0d6f5ebcb40416443009e850755d49ca42f
mauro <mauroparis96@gmail.com>
2016-09-03 23:20:54 -0300

    progress on ng-consult

--- app/scripts/directives/ngconsult.js
+++ app/scripts/directives/ngconsult.js
@@ -10,8 +10,5 @@ angular.module('aesculapiusFrontApp')
   .directive('ngConsult', function () {
     return {
       templateUrl: '/views/ngConsult.html',
-      // link: function postLink(scope, element, attrs) {
-      //   element.text('this is the ngConsult directive');
-      // }
     };
   });
