20a74150acaf570cee640eb597c30aca357d053d
mateosss <mateodemayo@gmail.com>
2016-09-15 10:29:56 -0300

    Added restangular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,9 +19,31 @@ angular
     'ngSanitize',
     'ngTouch',
     'ngMaterial',
-    'mdDataTable'
+    'mdDataTable',
+    'restangular'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $httpProvider, RestangularProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+    //set the base url for api calls on our RESTful services
+    var newBaseUrl = "";
+    if (window.location.hostname === "localhost") {
+      newBaseUrl = "http://localhost:8000/api/";
+    } else {
+      var deployedAt = window.location.href.substring(0, window.location.href);
+      newBaseUrl = deployedAt + "/api/";
+    }
+    RestangularProvider.setBaseUrl(newBaseUrl);
+    RestangularProvider.addResponseInterceptor(function(data, operation) {
+      var extractedData;
+      if (operation === "getList" && data.results) {
+        extractedData = data.results;
+      } else {
+        extractedData = data;
+      }
+      return extractedData;
+    });
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/angular-material-icons/angular-material-icons.min.js',
       'bower_components/mdDataTable/dist/md-data-table.js',
       'bower_components/mdDataTable/dist/md-data-table-templates.js',
+      'bower_components/restangular/dist/restangular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
