feb2bd9b5c4c883db731db3aa48b7e3bf101766a
Santiago <piojotissera@gmail.com>
2016-09-20 13:51:09 -0300

    Template Historia clinica

---
+++ app/scripts/controllers/history.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name aesculapiusFrontApp.controller:HistoryCtrl
+* @description
+* # HistoryCtrl
+* Controller of the aesculapiusFrontApp
+*/
+var DialogController = function ($scope, $mdDialog) {
+
+  $scope.consultList = [
+    {
+      Fecha: '2016-09-06T00:43:43.931Z',
+      Medico: 'Alfachoca Tracalanduda',
+      Descripcion: 'esto describe',
+    },
+    {
+      Fecha: '2016-09-06T00:43:43.931Z',
+      Medico: 'pamparama',
+      Descripcion: 'gripe y fiebre',
+    },
+
+
+  ];
+};
+
+
+angular.module('aesculapiusFrontApp')
+.controller('HistoryCtrl', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+  $scope.showTabDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'views/history.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      escapeToClose: true,
+    });
+  };
+}]);
