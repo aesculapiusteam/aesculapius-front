bf08bab18b5f42b28727ff5491a2e15679c37133
mateosss <mateodemayo@gmail.com>
2016-09-25 15:52:51 -0300

    favicon y some profile amb changes

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -15,7 +15,7 @@ var DialogController = function ($scope, $mdDialog) {
 
 angular.module('aesculapiusFrontApp')
 .controller('ProfileCtrl', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
-  
+
   $scope.showTabDialog = function(ev) {
     $mdDialog.show({
       controller: DialogController,
