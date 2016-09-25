1993c29ee13882e74a6f18452c1bec5854b90d9d
mateosss <mateodemayo@gmail.com>
2016-09-25 12:48:38 -0300

    ae-profile-icon css improvements

--- app/scripts/directives/ae-profile-icon.js
+++ app/scripts/directives/ae-profile-icon.js
@@ -9,10 +9,9 @@
 angular.module('aesculapiusFrontApp')
   .directive('aeProfileIcon', function () {
     return {
-      template: '<div style="margin:8px;width:{{size}}px;height:{{size}}px;background-color:#49F;float:left;border-radius:{{size/2}}px;color:white;font-size:{{size/2}}px;text-align:center;line-height:{{size}}px;">{{letter}}</div>',
+      template: '<div md-ink-ripple="true" md-whiteframe="2" class="unselectable" style="cursor:pointer;margin:8px;width:{{size}}px;height:{{size}}px;background-color:#49F;float:left;border-radius:{{size/2}}px;color:white;font-size:{{size/2}}px;text-align:center;line-height:{{size}}px;">{{letter}}</div>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        console.log(attrs);
         scope.size = attrs.size || 32;
         scope.letter = (attrs.name + "A").charAt(0);
       }
