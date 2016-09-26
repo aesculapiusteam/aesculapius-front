739b245f8fe3d30da018a467ad4ef3d0c9f196bb
mateosss <mateodemayo@gmail.com>
2016-09-26 18:54:39 -0300

    Probando aeData

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -11,10 +11,24 @@ angular.module('aesculapiusFrontApp')
 .controller('ProfileCtrl', [
   '$scope', '$mdDialog', '$rootScope', 'aeData',
   function ($scope, $mdDialog, $rootScope, aeData) {
+    $scope.profile = {};
 
-    $scope.profile = function() {
-      return aeData.profile;
-    };
+    // $scope.getProfile = function() {
+    //   // Caches the profile in $scope, also checks if profiles is an employee
+    //   // And if it is, makes a petition to get username, etc.
+    //   if (_.isEmpty($scope.profile)) {
+    //     console.log(aeData.profile);
+    //     if (aeData.profile.employee) {
+    //       console.log("ES EMPLOYEE");
+    //       //HACER PETICION A EMPLOYEES
+    //       // $scope.profile = aeData.profile;
+    //     } else {
+    //       console.log("NO LO ES EMPLOYEE");
+    //       $scope.profile = aeData.profile;
+    //     }
+    //   }
+    //   return $scope.profile;
+    // };
 
     $rootScope.cancel = function() {
       $mdDialog.cancel();
