27412b6548f2738de260e3488f5ce3a3b7a318fd
mauro <mauroparis96@gmail.com>
2016-10-06 20:11:00 -0300

    history on its way

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -7,38 +7,18 @@
 * # HistoryCtrl
 * Controller of the aesculapiusFrontApp
 */
-var DialogController = function ($scope, $mdDialog) {
-
-  $scope.consultList = [
-    {
-      Fecha: '2016-01-06T00:43:43.931Z',
-      Medico: 'Alfachoca Tracalanduda',
-      Descripcion: 'esto describe',
-    },
-    {
-      Fecha: '2016-09-06T00:43:43.931Z',
-      Medico: 'pamparama',
-      Descripcion: 'gripe y fiebre',
-    },
-
-
-  ];
-  $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
-};
-
-
 angular.module('aesculapiusFrontApp')
-.controller('HistoryCtrl', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
-  $scope.showTabDialog = function(ev) {
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: 'views/history.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true,
-      escapeToClose: true,
+.controller('HistoryCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
+  var allVisits = Restangular.all('visits');
+
+  $scope.getData = function (page, pageSize){
+    var offset = (page-1) * pageSize;
+    return allVisits.getList({pacient:6, limit: pageSize, offset:offset}).then(function(result){
+      return {
+        results: result,
+        totalResultCount: result.count
+      };
     });
   };
+
 }]);
