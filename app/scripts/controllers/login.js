07edff37cd98d3b92ed763481398d9a35b56b439
mateosss <mateodemayo@gmail.com>
2016-10-18 09:57:06 -0300

    Improved logo, login response message

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,8 +8,8 @@
  * Controller of the aesculapiusFrontApp
  */
 angular.module('aesculapiusFrontApp')
-  .controller('LoginCtrl', ['$scope', '$location', 'auth', '$rootScope',
-    function($scope, $location, auth, $rootScope) {
+  .controller('LoginCtrl', ['$scope', '$location', 'auth', '$rootScope', '$mdToast',
+    function($scope, $location, auth, $rootScope, $mdToast) {
       if (window.localStorage.token) {
         $location.path("/profiles"); //TODO DRY make this a succesfulLogin method or similar
       }
@@ -28,7 +28,12 @@ angular.module('aesculapiusFrontApp')
           },
           function(response) {
             $rootScope.loading = false;
-            console.log(response);
+            $mdToast.show(
+              $mdToast.simple()
+              .textContent(response.data.non_field_errors[0])
+              .position('bottom right')
+              .hideDelay(2000)
+            );
           }
         );
       };
