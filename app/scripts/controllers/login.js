e12516a31657368748a80d9a5c11942202e24afc
mateosss <mateodemayo@gmail.com>
2016-10-17 22:46:35 -0300

    Loading screen, more icons, more aesthetics

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,31 +1,36 @@
 'use strict';
 
 /**
-* @ngdoc function
-* @name aesculapiusFrontApp.controller:LoginCtrl
-* @description
-* # LoginCtrl
-* Controller of the aesculapiusFrontApp
-*/
+ * @ngdoc function
+ * @name aesculapiusFrontApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the aesculapiusFrontApp
+ */
 angular.module('aesculapiusFrontApp')
-.controller('LoginCtrl', ['$scope', '$location', 'auth',
-function ($scope, $location, auth) {
-  if (window.localStorage.token) {
-    $location.path("/profiles"); //TODO DRY make this a succesfulLogin method or similar
-  }
-  $scope.credentials = {
-    username: '',
-    password: ''
-  };
-  $scope.logIn = function() {
-    auth.token(this.credentials).then(
-      function(response) {
-        $location.path("/profiles");
-      },
-      function(response) {
-        console.log(response);
+  .controller('LoginCtrl', ['$scope', '$location', 'auth', '$rootScope',
+    function($scope, $location, auth, $rootScope) {
+      if (window.localStorage.token) {
+        $location.path("/profiles"); //TODO DRY make this a succesfulLogin method or similar
       }
-    );
-  };
-}
-]);
+
+      $scope.credentials = {
+        username: '',
+        password: ''
+      };
+
+      $scope.logIn = function() {
+        $rootScope.loading = true;
+        auth.token(this.credentials).then(
+          function() {
+            $rootScope.loading = false;
+            $location.path("/profiles");
+          },
+          function(response) {
+            $rootScope.loading = false;
+            console.log(response);
+          }
+        );
+      };
+    }
+  ]);
