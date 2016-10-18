2c11303a4f56583e01d487ec1ce156815b579720
mateosss <mateodemayo@gmail.com>
2016-10-18 11:49:15 -0300

    Added some doc to aeData

--- app/scripts/services/aedata.js
+++ app/scripts/services/aedata.js
@@ -9,11 +9,11 @@
  */
 angular.module('aesculapiusFrontApp')
   .service('aeData', function () {
-    this.me = null;
-    this.profile = null;
-    this.profiles = null;
-    this.employees = null;
-    this.visitObj = null;
-    this.reloadHistoryTable = null;
-    this.historyId = null;
+    this.me = null; // Current logged in restangular employee
+    this.profile = null; // Current selected restangular profile
+    this.profiles = null; // Current displayed restangular profiles in /profiles
+    this.employees = null; // Current displayed restangular employees in /employees
+    this.visitObj = null; // TODO Document this object
+    this.reloadHistoryTable = null; // Execute this function to reload the history table
+    this.historyId = null; // TODO Document this object
   });
