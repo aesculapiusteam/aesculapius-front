177bb18e4ec5580a6cde97ad986d2d8eff3b9d5e
mateosss <mateodemayo@gmail.com>
2016-10-24 22:32:23 -0300

    CHANGES FOR HOSTING AESCULAPIUS - READ MORE 1. Icons now are always downloaded, so if no internet connection, no icons. Even when developing. 2. Now the svg images are not being renamed in the Gruntfile.js because that bringed up some errors with the ng-svg-src prop. 3. Removed material icons from bower.

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}', //TODO Removed svg from the list
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
