4344736e73368d043e9b2ba11cfa7c598d5831f1
mateosss <mateodemayo@gmail.com>
2017-05-17 21:43:17 -0300

    Now after deploy.sh you are back to develop

--- deploy.sh
+++ deploy.sh
@@ -6,27 +6,29 @@ API_PROD="\"https:\/\/aesculapius.pythonanywhere.com\/api\/\""
 # TODO: Add option for $API_DEV = "https://aesculapiusdev.pythonanywhere.com/api/"
 # TODO: Add an option for custom commit message directly from deploy.sh
 
-echo "1/7 building aesculapius-front"
+echo "1/8 building aesculapius-front"
 grunt build
 
-echo "2/7 checkout to master"
+echo "2/8 checkout to master"
 git checkout master
 
-echo "3/7 changing api url"
+echo "3/8 changing api url"
 sed -i "s/\"http:\/\/[^;]*/$API_PROD/" dist/scripts/scripts.*
 
-echo "4/7 deleting old master files"
+echo "4/8 deleting old master files"
 rm -rf images/ scripts/ styles/ favicon.ico index.html manifest.json
 
-echo "5/7 placing new master files"
+echo "5/8 placing new master files"
 cp -r dist/* .
 
-echo "6/7 commiting..."
+echo "6/8 commiting..."
 git add --all
 git commit
 
-echo "7/7 pushing..."
+echo "7/8 pushing..."
 git push origin master
 
-echo "Finished."
+echo "8/8 back to develop"
+git checkout develop
 
+echo "Finished."
