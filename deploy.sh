2c9f6f9d79e0adc7af1cfd14fd89ad49160a8b34
mateosss <mateodemayo@gmail.com>
2017-05-17 22:47:56 -0300

    manifest.json won't bother anymore: just deleted, we only loose the initial background color and size but we gain less black magic and a working quasi pwapp

--- deploy.sh
+++ deploy.sh
@@ -7,33 +7,29 @@ API_PROD="\"https:\/\/aesculapius.pythonanywhere.com\/api\/\""
 # TODO: Add option for $API_DEV = "https://aesculapiusdev.pythonanywhere.com/api/"
 # TODO: Add an option for custom commit message directly from deploy.sh
 
-echo "1/9 building aesculapius-front"
+echo "1/8 building aesculapius-front"
 grunt build
 
-echo "2/9 checkout to master"
+echo "2/8 checkout to master"
 git checkout master
 
-echo "3/9 changing api url"
+echo "3/8 changing api url"
 sed -i "s/\"http:\/\/[^;]*/$API_PROD/" dist/scripts/scripts.*
 
-echo "4/9 changing manifest logo url"
-dist_icon=$(ls dist/images/logo_square_shadow.*.png | sed "s/.*images/images/" | sed 's/\//\\\//')
-sed -i "s/images\/logo_square_shadow.png/$dist_icon/" dist/manifest.json
+echo "4/8 deleting old master files"
+rm -rf images/ scripts/ styles/ favicon.ico index.html
 
-echo "5/9 deleting old master files"
-rm -rf images/ scripts/ styles/ favicon.ico index.html manifest.json
-
-echo "6/9 placing new master files"
+echo "5/8 placing new master files"
 cp -r dist/* .
 
-echo "7/9 commiting..."
+echo "6/8 commiting..."
 git add --all
 git commit
 
-echo "8/9 pushing..."
+echo "7/8 pushing..."
 git push origin master
 
-echo "9/9 back to develop"
+echo "8/8 back to develop"
 git checkout develop
 
 echo "Finished."
