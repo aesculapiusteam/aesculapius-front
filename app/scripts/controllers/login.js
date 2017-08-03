da3e8b3c364720621c525dbb64dc39fbd49f2a4e
mateosss <mateodemayo@gmail.com>
2017-08-03 13:12:34 -0300

    Added ?test parameter to login , now we can share aesculapius with other people by using ?test, ?test_doctor and ?test_secretary

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,8 +8,8 @@
  * Controller of the aesculapiusFrontApp
  */
 angular.module('aesculapiusFrontApp')
-  .controller('LoginCtrl', ['$scope', '$location', 'auth', '$rootScope', '$mdToast',
-    function($scope, $location, auth, $rootScope, $mdToast) {
+  .controller('LoginCtrl', ['$scope', '$location', 'auth', '$rootScope', '$mdToast', 'Restangular', '$routeParams', '$timeout',
+    function($scope, $location, auth, $rootScope, $mdToast, Restangular, $routeParams, $timeout) {
       if (window.localStorage.token) {
         $location.path("/profiles"); //TODO DRY make this a succesfulLogin method or similar
       }
@@ -19,9 +19,25 @@ angular.module('aesculapiusFrontApp')
         password: ''
       };
 
+      var test_doctor = {
+        username: 'test_doctor',
+        password: 'test_doctor',
+      };
+
+      var test_secretary = {
+        username: 'test_secretary',
+        password: 'test_secretary',
+      };
+
       $scope.logIn = function() {
         $rootScope.loading = true;
-        auth.token(this.credentials).then(
+
+        // Change api if testing
+        if (_.isEqual($scope.credentials, test_doctor) || _.isEqual($scope.credentials, test_secretary)) {
+          Restangular.setBaseUrl("https://aesculapiusdev.pythonanywhere.com/api/");
+        }
+
+        auth.token($scope.credentials).then(
           function() {
             $rootScope.loading = false;
             $location.path("/profiles");
@@ -37,5 +53,30 @@ angular.module('aesculapiusFrontApp')
           }
         );
       };
+
+      // Testing environment
+      if ($routeParams.test) {
+          $scope.isTesting = true;
+      }
+      if ($routeParams.test_doctor) {
+        $scope.isTesting = true;
+        $timeout(function(){$scope.credentials = test_doctor;}, 1000);
+        $timeout($scope.logIn, 2000);
+      } else if ($routeParams.test_secretary) {
+        $scope.isTesting = true;
+        $timeout(function(){$scope.credentials = test_secretary;}, 1000);
+        $timeout($scope.logIn, 2000);
+      }
+
+      $scope.testAs = function(role) {
+        if (role === 'doctor') {
+          $scope.credentials = test_doctor;
+          $timeout($scope.logIn, 250);
+        } else if (role === 'secretary') {
+          $scope.credentials = test_secretary;
+          $timeout($scope.logIn, 250);
+        }
+      };
+
     }
   ]);
