56ce5d1330ba6e6017fd5aa89b577176c871a4e1
mateosss <mateodemayo@gmail.com>
2017-08-03 11:57:18 -0300

    Added toast after new suggestion is created

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -126,8 +126,8 @@ angular
 
       $rootScope.showDialog = function(ev, scope, dontFetch) {
         if (scope && (scope.value || scope.value === 0)) {
-          if (!dontFetch){
-            if(aeData[ev.currentTarget.id + 's']){
+          if (!dontFetch) {
+            if (aeData[ev.currentTarget.id + 's']) {
               aeData[ev.currentTarget.id] = aeData[ev.currentTarget.id + 's'].get(scope.value).$object;
             } else {
               aeData[ev.currentTarget.id] = Restangular.one(ev.currentTarget.id + 's', scope.value).get().$object;
@@ -151,9 +151,9 @@ angular
           targetEvent: ev,
           clickOutsideToClose: true,
           escapeToClose: true,
-          onRemoving: function (){
-            if(!aeData.isConfirmThere){ // If confirm dialog is not there, then open it
-              if (scope && (scope.value || scope.value === 0)){
+          onRemoving: function() {
+            if (!aeData.isConfirmThere) { // If confirm dialog is not there, then open it
+              if (scope && (scope.value || scope.value === 0)) {
                 aeData.onDialogClose(aeData[ev.currentTarget.id], ev.currentTarget.id, scope.value);
               } else {
                 aeData.onDialogClose(aeData[ev.currentTarget.id], ev.currentTarget.id);
@@ -180,66 +180,67 @@ angular
           case 'delete':
             buttonText = 'Eliminar';
             cancelText = 'Cancelar';
-            if(products[0].name){ // Drug
+            if (products[0].name) { // Drug
               nexus = products.length === 1 ? ' este será removido' : ', estos serán removidos';
               text = 'Al eliminar ' + aeData.itemsInText(products) +
                 nexus + ' permanentemente del sistema.';
               toastConfirmText = aeData.itemsInText(products) + ' eliminados con exito!';
-            }else if(products[0].first_name || products[0].profile){ // Employees and Profiles
+            } else if (products[0].first_name || products[0].profile) { // Employees and Profiles
               text = 'Al eliminar a ' + aeData.nameOf(products[0]) +
-              ', este no aparecerá más en el sistema, sin embargo podrá ser restaurado luego.';
+                ', este no aparecerá más en el sistema, sin embargo podrá ser restaurado luego.';
               toastConfirmText = aeData.nameOf(products[0]) + ' eliminado con exito!';
-            }else{ // Visit
+            } else { // Visit
               text = 'Al eliminar la visita de ' + products[0].patient_name +
-              ', esta no aparecerá más en el sistema, sin embargo podrá ser restaurado luego.';
+                ', esta no aparecerá más en el sistema, sin embargo podrá ser restaurado luego.';
               toastConfirmText = 'Visita de ' + products[0].patient_name + ' eliminada con exito!';
             }
             break;
         }
         aeData.isConfirmThere = true; // the confirm dialog is open
 
-        var confirm = $mdDialog.confirm()//show the dialog!
+        var confirm = $mdDialog.confirm() //show the dialog!
           .title(text)
           .textContent('')
           .ariaLabel('confirm-dialog')
           .ok(buttonText)
           .cancel(cancelText);
 
-        $mdDialog.show(confirm).then(function() {// If OK
+        $mdDialog.show(confirm).then(function() { // If OK
           aeData.isConfirmThere = false;
-          if(action==='delete'){
-            var removeFn = function(x){// One function to delete them all, one function to rule them all
+          if (action === 'delete') {
+            var removeFn = function(x) { // One function to delete them all, one function to rule them all
               products[x].remove().then(
-                function(){
-                  aeData.reloadSelectedTable();//If the action has succeded reload selected table
+                function() {
+                  aeData.reloadSelectedTable(); //If the action has succeded reload selected table
                   $mdToast.show( // and show the toast
                     $mdToast.simple()
                     .textContent(toastConfirmText)
                     .position('bottom right')
                     .hideDelay(3000)
                   );
-                }, function(error){
-                  $rootScope.showActionToast('Lamentablemente hubo un error al eliminar '+ aeData.itemsInText(products),'error',
-                   error);
+                },
+                function(error) {
+                  $rootScope.showActionToast('Lamentablemente hubo un error al eliminar ' + aeData.itemsInText(products), 'error',
+                    error);
                 }
               );
             };
             for (var x = 0; x < products.length; x++) {
-              removeFn(x);//run function the times needed to delete all products
+              removeFn(x); //run function the times needed to delete all products
             }
           }
-        }, function() {//If CANCEL
+        }, function() { //If CANCEL
           aeData.isConfirmThere = false;
-          if(aeData.dialogInfo){
-            $rootScope.showDialog({'currentTarget':{'id':aeData.dialogInfo[0]}}, {'value':aeData.dialogInfo[1]}, true);
+          if (aeData.dialogInfo) {
+            $rootScope.showDialog({ 'currentTarget': { 'id': aeData.dialogInfo[0] } }, { 'value': aeData.dialogInfo[1] }, true);
           }
           aeData.reloadSelectedTable();
         });
       };
 
-      $rootScope.showAlert = function(errorText){
+      $rootScope.showAlert = function(errorText) {
         $mdDialog.show(
-        $mdDialog.alert()
+          $mdDialog.alert()
           .parent(angular.element(document.querySelector('#popupContainer')))
           .clickOutsideToClose(true)
           .title('Error')
@@ -249,24 +250,27 @@ angular
         );
       };
 
-      $rootScope.showActionToast = function(text, ev, scope, delay, notModal){
+      $rootScope.showActionToast = function(text, ev, scope, delay, notModal) {
         var toast = $mdToast.simple()
-      .textContent(text)
-      .action(notModal || 'VER')
-      .highlightAction(true)
-      .highlightClass('md-primary')
-      .position('bottom right')
-      .hideDelay(delay || 3000);
-
-      $mdToast.show(toast).then(function(response) {
-        if ( response === 'ok' ) {
-          if(ev === 'error'){
-            $rootScope.showAlert((scope.data && scope.data.detail) || scope);
-          }else{
-            $rootScope.showDialog(ev,scope);
-          }
+          .textContent(text)
+          .position('bottom right')
+          .hideDelay(delay || 3000);
+        if (!notModal) {
+          toast
+            .action(notModal || 'VER')
+            .highlightAction(true)
+            .highlightClass('md-primary');
         }
-      });
+
+        $mdToast.show(toast).then(function(response) {
+          if (response === 'ok') {
+            if (ev === 'error') {
+              $rootScope.showAlert((scope.data && scope.data.detail) || scope);
+            } else {
+              $rootScope.showDialog(ev, scope);
+            }
+          }
+        });
       };
     }
   ])
@@ -289,8 +293,9 @@ angular
       'A700': 'ff6d00',
       'contrastDefaultColor': 'light',
       'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-       '200', '300', '400', 'A100'],
-      'contrastLightColors': undefined    // could also specify this if default was 'dark'
+        '200', '300', '400', 'A100'
+      ],
+      'contrastLightColors': undefined // could also specify this if default was 'dark'
     });
 
     $mdThemingProvider.theme('default')
--- app/scripts/controllers/issue.js
+++ app/scripts/controllers/issue.js
@@ -8,14 +8,30 @@
  * Controller of the aesculapiusFrontApp
  */
 angular.module('aesculapiusFrontApp')
-  .controller('IssueCtrl', ['$scope', '$mdDialog', 'Restangular', function ($scope, $mdDialog, Restangular) {
-    $scope.cancel = function() {
-      $mdDialog.cancel();
-    };
-
-    $scope.save = function(){
-      $scope.issueForm.$submitted = true; //for the confirm dialog on close not to open
-      Restangular.all('issues').post($scope.issue);
-      $mdDialog.cancel();
-    };
-  }]);
+  .controller('IssueCtrl', ['$scope', '$rootScope', '$mdDialog', 'Restangular', 'aeData',
+    function($scope, $rootScope, $mdDialog, Restangular) {
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+      $scope.save = function() {
+        $scope.issueForm.$submitted = true; //for the confirm dialog on close not to open
+        Restangular.all('issues').post($scope.issue).then(
+          function(response) {
+            $rootScope.showActionToast(
+              '¡Gracias por la sugerencia!',
+              { 'currentTarget': { 'id': 'issue' } },
+              { 'value': response.id },
+              3000,
+              true
+            );
+            $mdDialog.cancel();
+          },
+          function(error) {
+            $mdDialog.cancel();
+            $rootScope.showActionToast('Lamentablemente hubo un error al editar la consulta.', 'error',
+              error);
+          }
+        );
+      };
+    }
+  ]);
