87b932939b3f04e70e1e9dc33d5bd44fa678e156
mateosss <mateodemayo@gmail.com>
2018-03-17 15:13:11 -0300

    Added button redirecting to the user manual

--- app/scripts/controllers/base.js
+++ app/scripts/controllers/base.js
@@ -8,8 +8,8 @@
  * Controller of the aesculapiusFrontApp
  */
 angular.module('aesculapiusFrontApp')
-  .controller('BaseCtrl', ['$scope', '$location', 'auth', 'aeData', '$rootScope', '$mdMenu',
-    function($scope, $location, auth, aeData, $rootScope, $mdMenu) {
+  .controller('BaseCtrl', ['$scope', '$location', 'auth', 'aeData', '$rootScope', '$mdMenu', '$window',
+    function($scope, $location, auth, aeData, $rootScope, $mdMenu, $window) {
 
       $scope.me = aeData.me;
       $scope.currentView = "profiles";
@@ -25,6 +25,10 @@ angular.module('aesculapiusFrontApp')
       $scope.logout = function() {
         auth.logout();
         $location.path("/login");
+      };
+
+      $scope.openManual = function() {
+        $window.open('https://docs.google.com/document/d/1Zhz7KRrrfJcfBtfEPrP8LC0ECz6LPUeHdw6JpBrfIQI/preview', '_blank');
       };
 
       $scope.mdCloseMenu = function(isOpen) {
